e5e24a011dadb83477238cefd487c024f214a4ad
ChrisProgramming2018 <info2016frei@gmail.com>
2018-11-15 18:02:23 +0100

    add  main file

---
+++ assignment-2/WhitMondayMain.cpp
@@ -0,0 +1,24 @@
+// Copyright 2018
+// Computer Science
+// Author: Christian Leininger  Christianleininger@gmx.de
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "./WhitMonday.h"
+
+int main(int argc, char** argv) {
+  // Prints if you dont give a Year
+  if (argc != 2) {
+    printf("Usage ./Whitmonday < Year>\n");
+    exit(1);
+  }
+  int year = atoi(argv[1]);
+  int easter = calculate(year);
+  if (easter > 92) {  // from 1.3. to 1.06 are 92
+    int dayInJuni = easter - 92;
+    printf(" %d.Juni.%d\n", dayInJuni, year);
+  } else {
+    int dayInMay = easter - 61;   // from 1.3 to 1.06 are 61
+    printf(" %d.Mai.%d\n", dayInMay, year);
+  }
+}
