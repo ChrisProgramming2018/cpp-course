73810c5efff69aa38414ebd2b0376c9d04c7527a
ChrisProgramming2018 <info2016frei@gmail.com>
2018-11-15 13:31:17 +0100

    add  .h file

---
+++ assignment-2/WhitMonday.h
@@ -0,0 +1,14 @@
+// Copyright 2018
+// Computer Science
+// Author: Christian Leininger Christianleininger@gmx.de
+
+
+#ifndef BLATT_02_WHITMONDAY_H_
+#define BLATT_02_WHITMONDAY_H_
+
+// Using the Algoritm from Gauß that Calculate the Eastersunday
+// for the greoriansichen Calendar
+// Function to calculate
+int calculate(int year);
+
+#endif  // BLATT_02_WHITMONDAY_H_
