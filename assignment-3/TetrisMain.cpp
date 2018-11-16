8bd42038fd25bd4c52a6df16f250a422c762a20e
ChrisProgramming2018 <info2016frei@gmail.com>
2018-11-16 19:55:32 +0100

    add Main for Tetris

---
+++ assignment-3/TetrisMain.cpp
@@ -0,0 +1,29 @@
+// Copyright 2016, University of Freiburg,
+// Computer Sience
+// Author: Christian Leininger
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+#include "./Tetris.h"
+
+// Main function.
+int main(int argc, char** argv) {
+  int key;
+  int Timer = 0;
+  initializeScreen();
+  showTetromino(true);
+  while (true) {
+    key = getch();
+    if (++Timer % 100 == 0) key = KEY_DOWN;
+    showTetromino(false);
+    moveTetromino(key);
+    showTetromino(true);
+    fflush(stdout);
+    usleep(10 * 1000);
+    if (ty == 19) ty = 1;
+  }
+  endwin();
+}
