912df07e7f067541ff10f1759a5c88ba55f97050
ChrisProgramming2018 <info2016frei@gmail.com>
2018-11-22 17:23:56 +0100

    add function append

--- assignment-6/String.cpp
+++ assignment-6/String.cpp
@@ -29,3 +29,29 @@ String::~String() {
 int String::length() const {
   return _length;
 }
+
+// ________________________________________________________
+void String::set(const char* s) {
+  _length = strlen(s);
+  delete[] = _contents;
+  _contents = new char[_length];
+  for (int i = 0; i < _length; i++) {
+    _contentsi[i] = s[i];
+  }
+}
+
+// ________________________________________________________
+void String::append(const String& s) {
+  int size = _length + s._length;
+  char* new_content = new char[size];
+  for (int i = 0; i < _length; i++) {
+    new_content[i] = _contents[i];
+  }
+
+  for (int j = 0; j < s._length; j++) {
+     new_content[_length + j] = s._contents[j];
+  }
+  delete[] = _contents;
+  _contents = new_content;
+  _length = size;
+}
