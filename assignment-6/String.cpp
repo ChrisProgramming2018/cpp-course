72a379235ce8cf5cb7fe27967873346d32e5ac49
ChrisProgramming2018 <info2016frei@gmail.com>
2018-11-22 16:55:12 +0100

    add function deepcopy

--- assignment-6/String.cpp
+++ assignment-6/String.cpp
@@ -1,7 +1,6 @@
-//
-//
-//
-
+// Copyright 2018
+// Computer Science
+// Author: Christian Leininger Christianleininger@gmx.de
 
 #include <stdio>
 #include "./String.h"
@@ -15,5 +14,18 @@ String::String() {
 // ________________________________________________________
 String::String(const String& s) {
   _length = s._length;
-  _contents = new char [_length];
+  _contents = new char[_length];
+  for (int i = 0; i < _length; i++) {
+    _contents[i] = s.contents[i];
+  }
+}
+
+// ________________________________________________________
+String::~String() {
+  delete[] _contents;
+}
+
+// ________________________________________________________
+int String::length() const {
+  return _length;
 }
