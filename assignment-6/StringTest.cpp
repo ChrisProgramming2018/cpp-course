73d188d965b2b8b7ed86c0a884082d86f1c512d3
ChrisProgramming2018 <info2016frei@gmail.com>
2018-11-24 13:09:15 +0100

    add test append

--- assignment-6/StringTest.cpp
+++ assignment-6/StringTest.cpp
@@ -35,3 +35,28 @@ TEST(StringTest, DeepCopy) {
   ASSERT_EQ('o', s3._contents[2]);
   ASSERT_EQ('f', s3._contents[3]);
 }
+
+// _________________________________________________________
+TEST(StringTest, append) {
+  String s;
+  s.set("Computer");
+  String s2;
+  s2.set(" Science");
+  s.append(s2);
+  ASSERT_EQ('C', s._contents[0]);
+  ASSERT_EQ('o', s._contents[1]);
+  ASSERT_EQ('m', s._contents[2]);
+  ASSERT_EQ('p', s._contents[3]);
+  ASSERT_EQ('u', s._contents[4]);
+  ASSERT_EQ('t', s._contents[5]);
+  ASSERT_EQ('e', s._contents[6]);
+  ASSERT_EQ('r', s._contents[7]);
+  ASSERT_EQ(' ', s._contents[8]);
+  ASSERT_EQ('S', s._contents[9]);
+  ASSERT_EQ('c', s._contents[10]);
+  ASSERT_EQ('i', s._contents[11]);
+  ASSERT_EQ('e', s._contents[12]);
+  ASSERT_EQ('n', s._contents[13]);
+  ASSERT_EQ('c', s._contents[14]);
+  ASSERT_EQ('e', s._contents[15]);
+}
