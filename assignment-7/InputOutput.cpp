9b251a4f18fd13f791ca78e4c242957e705cb90b
ChrisProgramming2018 <info2016frei@gmail.com>
2018-11-25 13:56:46 +0100

    add InputOut function for parse

--- assignment-7/InputOutput.cpp
+++ assignment-7/InputOutput.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getop.h>
+#include <iostream>
 #include "./String.h"
 #include "./InputOutput.h"
 
@@ -16,4 +17,22 @@ InputOutput::InputOutput() {
 
 // ________________________________________________________
 void InputOutput::parseCommandLineArguments(int argc, char** argv) {
-  struct option options [] = { 
+  struct option options [] = {
+    { "--palindroms-only", 0, NULL, 'p'},
+    { NULL, 0, NULL, 0 }
+  };
+  optind = 1;
+  while (true) {
+    char c = getopt_longi(argc, argv, "p", options, NULL);
+    std::cout << c << std::endl;
+    if ( c== -1) break;
+    if ( c== '?') { exit(1)};
+    if ( c== 'p') _palendromes = true;
+  }
+  if ( optind + 1 != argc) {
+    fprintf(stderr, "Usage: ./InputOutputMain <filename>\n");
+    exit(1);
+  }
+  _filename = argv[optind];
+}
+
