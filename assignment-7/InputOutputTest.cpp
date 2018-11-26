def877effd690c69b9b82cde5cb092f36d29b705
ChrisProgramming2018 <info2016frei@gmail.com>
2018-11-26 16:44:11 +0100

    add test file

---
+++ assignment-7/InputOutputTest.cpp
@@ -0,0 +1,11 @@
+// Copyright 2018
+// Computer Science
+// Author : Christian Leininger christianleininger@gmx.de
+
+
+#include <gtest/gtest.h>
+#include "./InputOutput.h"
+
+// ________________________________________________________
+TEST(InputOutputTest, parseCommandLineArguments) {
+}
