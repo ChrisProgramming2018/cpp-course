b99ed8fe7c1927641d43a058bc9afb979c7ef797
ChrisProgramming2018 <info2016frei@gmail.com>
2018-11-27 15:18:17 +0100

    add more tests

--- assignment-7/InputOutputTest.cpp
+++ assignment-7/InputOutputTest.cpp
@@ -8,4 +8,39 @@
 
 // ________________________________________________________
 TEST(InputOutputTest, parseCommandLineArguments) {
+  InputOutput io;
+  int argc = 2;
+  char* argv[2] = {
+    const_cast<char*>("./ReverterMain"),
+    const_cast<char*>("t.txt")
+  };
+  io.parseCommandLineArguments(argc, argv);
+  ASSERT_STREQ("t.txt", io._filename);
+  ASSERT_FALSE(io._palindromes);
+}
+
+// ________________________________________________________
+TEST(InputOutputTest, parseCommandLineArguments2) {
+  InputOutput io;
+  int argc = 1;
+  char* argv[1] = {
+    const_cast<char*>("./InputOutputMain ") };
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  ASSERT_DEATH(io.parseCommandLineArguments(argc, argv), "Usage:.*");
+}
+// ________________________________________________________
+TEST(InputOutputTest, process) {
+  InputOutput io;
+  int argc = 5;
+  char* argv[5] = {
+    const_cast<char*>("./ReverterMain"),
+    const_cast<char*>("-p"),
+    const_cast<char*>("test.txt"),
+    const_cast<char*>(" > "),
+    const_cast<char*>("outputtest.txt")
+  };
+  io.parseCommandLineArguments(argc, argv);
+  ASSERT_STREQ("test.txt", io._filename);
+  ASSERT_TRUE(io._palindromes);
+  io.process();
 }
