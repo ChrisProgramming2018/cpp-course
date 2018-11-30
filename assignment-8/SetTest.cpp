5b574abd49ad34d45a7e284165835d7e3aa29941
ChrisProgramming2018 <info2016frei@gmail.com>
2018-11-30 14:43:42 +0100

    add more tests

--- assignment-8/SetTest.cpp
+++ assignment-8/SetTest.cpp
@@ -13,3 +13,25 @@ TEST(SetTest, constructorT) {
 }
 
 // ____________________________________________________________________________
+TEST(SetTest, insertT) {
+  Set<int> set;
+  ASSERT_EQ(0, set._size);
+  set.insert(0);
+  ASSERT_EQ(1, set._size);
+  ASSERT_EQ(0, set._elements[0]);
+
+  set.insert(0);
+  ASSERT_EQ(1, set._size);
+  ASSERT_EQ(0, set._elements[0]);
+
+  set.insert(2);
+  ASSERT_EQ(2, set._size);
+  ASSERT_EQ(0, set._elements[0]);
+  ASSERT_EQ(2, set._elements[1]);
+
+  set.insert(1);
+  ASSERT_EQ(3, set._size);
+  ASSERT_EQ(0, set._elements[0]);
+  ASSERT_EQ(2, set._elements[1]);
+  ASSERT_EQ(1, set._elements[2]);
+}
