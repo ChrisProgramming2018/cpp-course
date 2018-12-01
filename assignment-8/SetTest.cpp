e6112f444b49e02c10f8331ba5e4766c95865321
ChrisProgramming2018 <info2016frei@gmail.com>
2018-12-01 10:16:37 +0100

    add test for remove

--- assignment-8/SetTest.cpp
+++ assignment-8/SetTest.cpp
@@ -35,3 +35,50 @@ TEST(SetTest, insertT) {
   ASSERT_EQ(2, set._elements[1]);
   ASSERT_EQ(1, set._elements[2]);
 }
+
+// ____________________________________________________________________________
+TEST(SetTest, removeT) {
+  Set<int> set;
+  set.insert(0);
+  set.insert(0);
+  set.insert(1);
+  set.insert(2);
+  ASSERT_EQ(3, set._size);
+  ASSERT_EQ(0, set._elements[0]);
+  ASSERT_EQ(1, set._elements[1]);
+  ASSERT_EQ(2, set._elements[2]);
+
+  set.remove(0);
+  ASSERT_EQ(2, set._size);
+  ASSERT_EQ(1, set._elements[0]);
+  ASSERT_EQ(2, set._elements[1]);
+
+  set.remove(0);
+  ASSERT_EQ(2, set._size);
+  ASSERT_EQ(1, set._elements[0]);
+  ASSERT_EQ(2, set._elements[1]);
+
+  set.remove(1);
+  ASSERT_EQ(1, set._size);
+  ASSERT_EQ(2, set._elements[0]);
+}
+
+// ____________________________________________________________________________
+TEST(SetTest, findT) {
+  Set<int> set;
+  ASSERT_FALSE(set.find(0));
+  ASSERT_FALSE(set.find(1));
+
+  set.insert(0);
+  ASSERT_TRUE(set.find(0));
+  ASSERT_FALSE(set.find(1));
+
+  set.insert(1);
+  ASSERT_TRUE(set.find(0));
+  ASSERT_TRUE(set.find(1));
+
+  set.remove(0);
+  ASSERT_TRUE(set.find(1));
+  ASSERT_FALSE(set.find(0));
+}
+
