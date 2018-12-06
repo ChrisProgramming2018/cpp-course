afc97040e1a1dd3ef024aafd4d87f6211291b125
ChrisProgramming2018 <info2016frei@gmail.com>
2018-12-06 10:23:44 +0100

    add Test

---
+++ assignment-9/WordCounterTest.cpp
@@ -0,0 +1,22 @@
+// Copyright 2018
+// Computer Science
+// Author: Christian Leininger christianleininger@gmx.de
+
+#include <gtest/gtest.h>
+#include <string>
+#include <utility>
+#include <vector>
+#include "./WordCounter.h"
+
+
+// ____________________________________________________________________________
+TEST(WordCounterTest, readFile) {
+  FILE* input = fopen("WordCounterTest.tmp", "w");
+  fprintf(input,"test test more\n nextline\n end\n");
+  fclose(input);
+
+  WordCounter wc;
+
+  wc.readFile(std::string("WordCounterTest.tmp"));
+  ASSERT_EQ(4, wc._words);
+}
