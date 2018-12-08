a7eda016febbcd177ae04ab7f1abb08e0f69bba4
ChrisProgramming2018 <info2016frei@gmail.com>
2018-12-08 15:30:39 +0100

    add Movie h file

---
+++ assignment-10/Movie.h
@@ -0,0 +1,17 @@
+// Copyright 2018
+// Computer Sicence
+// Autor: Christian Leininger christianleininger@gmx.de
+
+#ifndef
+#define
+
+#include <gtest/gtest.h>
+#include <string>
+class Movie {
+ public:
+
+ private:
+
+};
+
+#endif  // 
