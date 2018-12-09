db3608828d64efc437ca00449646f304ffb81341
ChrisProgramming2018 <info2016frei@gmail.com>
2018-12-09 14:21:44 +0100

    extand h file

--- assignment-10/Movie.h
+++ assignment-10/Movie.h
@@ -2,19 +2,29 @@
 // Computer Sicence
 // Autor: Christian Leininger christianleininger@gmx.de
 
-#ifndef
-#define
+#ifndef ASSIGNMENT_10_MOVIE_H_
+#define ASSIGNMENT_10_MOVIE_H_
 
 #include <gtest/gtest.h>
 #include <string>
 class Movie {
  public:
   Movie(const std::string title, const int year);
-  
-  // frees the memory
-  virtual ~ Movie() {} 
- private:
 
+  // frees the memory
+  virtual ~Movie() {}
+
+  bool match(const std::string  pointer) const;
+
+  // show inforamtion readable form
+  virtual std::string toString() const;
+
+ private:
+  // Movie title
+  std::string _title;
+
+  // year of release
+  int _year;
 };
 
-#endif  // 
+#endif  // ASSIGNMENT_10_MOVIE_H_
