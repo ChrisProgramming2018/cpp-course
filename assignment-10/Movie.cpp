b55455b47c9589248e6fc409550830632f67e39d
ChrisProgramming2018 <info2016frei@gmail.com>
2018-12-10 09:33:16 +0100

    add to string method

---
+++ assignment-10/Movie.cpp
@@ -0,0 +1,24 @@
+// Copyright 2018
+// Computer Science
+// Autor: Christian Leininger christianleininger@gmx.de
+
+
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include "./Movie.h"
+
+
+// ____________________________________________________________________________
+Movie::Movie(const std::string title, const int year) {
+  _title = title;
+  _year = year;
+}
+
+
+// _____________________________________________________________________________
+std::string Movie::toString() const {
+  std::ostringstream oss;
+  oss << "\"" << _title << "\"" << " " << "(" << _year << ")";
+  return oss.str();
+}
