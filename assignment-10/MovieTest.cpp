652f1983073cd32635552d01b21414cf65bee92f
ChrisProgramming2018 <info2016frei@gmail.com>
2018-12-13 12:57:43 +0100

    add new tests

--- assignment-10/MovieTest.cpp
+++ assignment-10/MovieTest.cpp
@@ -8,8 +8,18 @@
 #include "./Movie.h"
 
 // Movie
+// ____________________________________________________________________________
 TEST(MovieTest, constructor) {
   Movie m("MyTitle", 2016);
   ASSERT_STREQ("MyTitle", m._title.c_str());
   ASSERT_EQ(2016, m._year);
+}
+
+// ____________________________________________________________________________
+TEST(MovieTest, matches) {
+  Movie m("MyTitle", 2016);
+  ASSERT_FALSE(m.matches("2016"));
+  ASSERT_TRUE(m.matches("yti"));
+  ASSERT_FALSE(m.matches("yTi"));
+  ASSERT_TRUE(m.matches(""));
 }
