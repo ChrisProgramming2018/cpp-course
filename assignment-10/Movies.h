d7feb935359b1f408e4a603017276608efb9f2d5
ChrisProgramming2018 <info2016frei@gmail.com>
2018-12-14 14:45:39 +0100

    add Movies h file

---
+++ assignment-10/Movies.h
@@ -0,0 +1,18 @@
+// Copyright 2018
+// Computer Sicence
+// Autor: Christian Leininger christianleininger@gmx.de
+   
+#ifndef ASSIGNMENT_10_MOVIES_H_
+#define ASSIGNMENT_10_MOVIES_H_
+
+#include <gtest/gtest.h>
+#include <string>
+ 
+class Movies {
+ public:
+   
+
+ private:
+  void Movies::readFile(const std::string filename);
+
+};
