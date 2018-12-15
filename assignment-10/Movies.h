f87132ff22cd29d9a6ad05ec6e3c933a64f3beaf
ChrisProgramming2018 <info2016frei@gmail.com>
2018-12-15 20:13:55 +0100

    add new h file to read file

--- assignment-10/Movies.h
+++ assignment-10/Movies.h
@@ -1,18 +1,30 @@
 // Copyright 2018
 // Computer Sicence
 // Autor: Christian Leininger christianleininger@gmx.de
-   
+
 #ifndef ASSIGNMENT_10_MOVIES_H_
 #define ASSIGNMENT_10_MOVIES_H_
 
 #include <gtest/gtest.h>
 #include <string>
- 
+#include <vector>
+#include "./Movie.h"
+
+
 class Movies {
  public:
-   
+  // deconstructor free memory
+  ~Movies();
+  // Returns matching movies sorted by title.
+  std::vector<Movie*> findMovies(const std::string match) const;
+  FRIEND_TEST(MoviesTest, findMovies);
+
+  // parse file into vector
+  void readFile(const std::string filename);
+  FRIEND_TEST(MoviesTest, readFile);
 
  private:
-  void Movies::readFile(const std::string filename);
-
+  // save all Movie instances
+  std::vector<Movie*> _movies;
 };
+#endif  // ASSIGNMENT_10_MOVIES_H_
