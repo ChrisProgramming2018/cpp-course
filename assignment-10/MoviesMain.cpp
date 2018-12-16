b77b0fe26247064740920f7abe0a67bad21b9e98
ChrisProgramming2018 <info2016frei@gmail.com>
2018-12-16 14:43:29 +0100

    add intervace

--- assignment-10/MoviesMain.cpp
+++ assignment-10/MoviesMain.cpp
@@ -19,4 +19,16 @@ int main(int argc, char** argv) {
   std::cout << "Loading movies ";
   movies.readFile(std::string(argv[1]));
   std::cout << "done." << std::endl;
+
+  while (true) {
+    std::string keyWord;
+    std::cout << "Type query or \e[1mquit\e[0m to exit." << std::endl;
+    std::cout << "\e[1m> \e[0m";
+    std::getline(std::cin, keyWord);
+    if (keyWord == "quit") { break; }
+    for (auto& movie : movies.findMovies(keyWord)) {
+      std::cout << movie->toString() << std::endl;
+    }
+    std::cout << std::endl;
+  }
 }
