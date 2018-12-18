b1bb0a3258bfe0e0e358db6313532d2d6c377210
ChrisProgramming2018 <info2016frei@gmail.com>
2018-12-18 16:51:28 +0100

    add function get movie by year

--- assignment-10/MoviesMain.cpp
+++ assignment-10/MoviesMain.cpp
@@ -21,20 +21,29 @@ int main(int argc, char** argv) {
   std::cout << "done." << std::endl;
   std::vector<Movie*> result;
   result = movies.getMovies();
-  std::cout <<"Size of databais is "  << result.size() <<" movies" <<std::endl;
-  std::cout <<"File needs to be in format movietitle tab year tab ... " <<std::endl;
-  for (auto& movie : movies._movies) {
-      // std::cout << movie->getTitle() << std::endl;
-      //std::cout << movie->toString() << std::endl;
-  }
+  std::cout << "Size of databais is "  << result.size() <<
+    " movies" << std::endl;
+  std::cout <<
+    "File needs to be in format movietitle tab year tab ... " << std::endl;
   while (true) {
     std::string keyWord;
-    std::cout << "Type query or \e[1mquit\e[0m to exit." << std::endl;
+    std::cout << "Type query or \e[1mq\e[0m to exit." << std::endl;
+    std::cout << "Type the year to get all movies from that year" << std::endl;
     std::cout << "\e[1m> \e[0m";
     std::getline(std::cin, keyWord);
-    if (keyWord == "quit") { break; }
-    for (auto& movie : movies.findMovies(keyWord)) {
-      std::cout << movie->toString() << std::endl;
+    if (keyWord == "q") { break; }
+    if (!keyWord.empty()
+      && keyWord.find_first_not_of("0123456789") == std::string::npos) {
+      std::cout << keyWord << std::endl;
+      int year = std::stoi(keyWord);
+      result = movies.yearMovies(year);
+      if (result.size() == 0) {
+        std::cout << "No movie found with the year" << std::endl;
+        continue;
+      }
+      for (auto& movie : movies.yearMovies(year)) {
+        std::cout << movie->toString() << std::endl;
+      }
     }
     std::cout << std::endl;
   }
